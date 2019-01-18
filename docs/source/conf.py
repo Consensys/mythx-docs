ccbab72115bb6e285a02f40d41e8881a045ef660
R. Bernstein <rocky@users.noreply.github.com>
2019-01-18 03:09:37 -0500

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,7 +26,7 @@ author = 'The MythX Team'
 version = ''
 # The full version, including alpha/beta/rc tags
 
-release = '0.0.2
+release = '0.0.2'
 
 # -- General configuration ---------------------------------------------------
 
