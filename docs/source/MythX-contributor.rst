1290b2599393b6143323d9f831049220aa60f868
rocky <rb@dustyfeet.com>
2019-01-17 21:31:22 -0500

    Misc lingering merge commit problems

--- docs/source/MythX-contributor.rst
+++ docs/source/MythX-contributor.rst
@@ -6,4 +6,5 @@ Contributing to MythX
 So you are an individual developer and you want to join this cool new MythX revolution? This is the section for you.
 
 * Gitcoin bounties.
+* Discord Channels
 * Links to "how to contribute" docs
--- docs/source/conf.py
+++ docs/source/conf.py
@@ -16,11 +16,6 @@
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
-# Allow markdown
-from recommonmark.parser import CommonMarkParser
-source_parsers = {'.md': CommonMarkParser}
-
-
 # -- Project information -----------------------------------------------------
 
 project = 'MythX Contributor, Developer, and Partner Guide'
